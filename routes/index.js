a16765558614408c03f7239146370dd33b148b92
Gerardo O Figueroa <gerardo_ofc@yahoo.com>
2017-03-29 02:15:34 +0000

    Changed interface

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HealthNet Web GUI' });
+  res.render('index', { title: 'HealthNet Web GUI', name: 'John Smith' });
 });
 
 module.exports = router;
